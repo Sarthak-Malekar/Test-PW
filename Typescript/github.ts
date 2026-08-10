e2a4e36f842f71ab50a9d6389ba2b23dccf65aac
Sarthak-Malekar <sarthakmalekar1999@gmail.com>
2026-08-10 21:32:54 +0530

    Added github

--- Typescript/github.ts
+++ Typescript/github.ts
@@ -6,4 +6,6 @@
 
     //1- Create a new repository on the version control tool.
     //2- Create a new project and write the test cases in your local system.
-    //3- git init - Initialize a new git repo inside your local system.
+    //3- "git init" - Initialize a new git repo inside your local system.
+    //4- "git add ." - Add all the files that you have added/changed in your local git area
+    //5- "git commit -m "first commit" " - Commit all the changes to the local git with a message.
