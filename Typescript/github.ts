8decb66d3058b3e0100b758a6b9701aa91b69886
Sarthak-Malekar <sarthakmalekar1999@gmail.com>
2026-09-12 17:34:56 +0530

    Updated github.ts

--- Typescript/github.ts
+++ Typescript/github.ts
@@ -9,3 +9,8 @@
     //3- "git init" - Initialize a new git repo inside your local system.
     //4- "git add ." - Add all the files that you have added/changed in your local git area.
     //5- "git commit -m "first commit" " - Commit all the changes to the local git with a message.
+    //6- "git branch -M master" - Create a new branch named as "master".
+    //7- "git branch" - List all the branches that are available in your repository.(local system)
+    //8- "git remote add origin https://github.com/Sarthak-Malekar/Test-PW.git" - Adds a remote repo to your 
+    //    local repo and link it with the remote repo.
+    //9- "git push -u origin Master" - Push the committed changes from local "Master" branch to remote repository.
